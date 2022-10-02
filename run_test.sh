115e0f136891753dd3842118a725ea12eef05865
Florian PAUTOT <florian.pautot@gmail.com>
2022-10-02 23:45:19 +0200

    add seeding to run test script

--- run_test.sh
+++ run_test.sh
@@ -9,4 +9,17 @@ else
   TEST_BUILD_PATH=${BUILD_PATH}/${TESTS_PATH}/*.js
 fi
 
+#echo "Seeding accounts ..."
+#completium-cli transfer 10tz from alice to carl --force
+#completium-cli transfer 10tz from alice to daniel --force
+#completium-cli transfer 10tz from alice to eddy --force
+#completium-cli transfer 10tz from alice to flo --force
+#completium-cli transfer 10tz from alice to gary --force
+#completium-cli transfer 10tz from alice to hugo --force
+#completium-cli transfer 10tz from alice to ian --force
+#completium-cli transfer 10tz from alice to jacky --force
+#completium-cli transfer 10tz from alice to kevin --force
+#completium-cli transfer 10tz from alice to tz1KqTpEZ7Yob7QbPE4Hy4Wo8fHG8LhKxZSx --force
+
+echo "Running tests ..."
 rm -rf ${BUILD_PATH} && npx tsc --outDir ${BUILD_PATH} && mocha --timeout 0 --slow 99999999999999999 ${TEST_BUILD_PATH}
